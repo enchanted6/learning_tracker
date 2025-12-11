dbe58ca62fcab536f3ad8b12d3aeb5b9ffba368b
liusiyang <liu767606@outlook.com>
2025-12-11 11:06:23 +0800

    feat:后台管理和测试

--- learning_tracker/tracker/admin.py
+++ learning_tracker/tracker/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Course,StudySession,KnowledgePoint # 导入数据库模型
 
 # Register your models here.
+# 注册模型
+admin.site.register(Course)
+admin.site.register(StudySession)
+admin.site.register(KnowledgePoint)
+
+
+
+
